1a8a13e57d1938deda178f307496024d816d7711
Janne Selkälä <janne_selkala@hotmail.com>
2013-05-06 20:40:15 +0200

    Halvfärdig power up klass

---
+++ src/kth/csc/inda/ads/Powerup.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package kth.csc.inda.ads;
+
+import java.awt.Color;
+
+/**
+ * @author Janne
+ *
+ */
+public class Powerup implements ActAndDraw {
+
+	/* (non-Javadoc)
+	 * @see kth.csc.inda.ads.ActAndDraw#act(kth.csc.inda.ads.NSnake)
+	 */
+	@Override
+	public boolean act(NSnake snake) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see kth.csc.inda.ads.ActAndDraw#getColor()
+	 */
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
